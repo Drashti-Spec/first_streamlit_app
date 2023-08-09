d5242665a5fc362b74c6ba9d17d0317f6fd5e55f
Drashti-Spec <96901573+Drashti-Spec@users.noreply.github.com>
2023-08-09 17:59:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Bread Butter')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Poha')
